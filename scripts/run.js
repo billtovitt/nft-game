5bc8088092313dec6b35a92f26d19cb38e04a10e
vecnazmaga <103976145+vecnazmaga@users.noreply.github.com>
2022-07-16 08:02:20 -0400

    first commit

---
+++ scripts/run.js
@@ -0,0 +1,47 @@
+const main = async () => {
+  const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
+
+  const gameContract = await gameContractFactory.deploy(
+    ["Leo", "Aang", "Pikachu"],
+    [
+      "https://i.imgur.com/pKd5Sdk.png",
+      "https://i.imgur.com/xVu4vFL.png",
+      "https://i.imgur.com/u7T87A6.png",
+    ],
+    [100, 200, 300],
+    [100, 50, 25],
+    "Elon Musk",
+    "https://i.imgur.com/AksR0tt.png",
+    10000,
+    50
+  );
+
+  await gameContract.deployed();
+  console.log("Contract deployed to:", gameContract.address);
+
+  let txn;
+  // We only have three characters.
+  // an NFT w/ the character at index 2 of our array.
+  txn = await gameContract.mintCharacterNFT(2);
+  await txn.wait();
+
+  txn = await gameContract.attackBoss();
+  await txn.wait();
+
+  txn = await gameContract.attackBoss();
+  await txn.wait();
+
+  console.log("Done!");
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
